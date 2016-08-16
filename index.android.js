b5510c430ceb504da7664d9886e7d2e9ae3740c2
unknown <>
2016-08-16 12:32:06 +0800

    update file

---
+++ index.android.js
@@ -0,0 +1,6 @@
+'use strict';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './app/containers/app';
+
+AppRegistry.registerComponent('MyProject', () => App);
